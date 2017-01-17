b4e7f6a67b094e5539dd3f9257917c3b51de5122
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-01-17 12:59:43 -0500

    Add first routes
    Also update .json file with gulp and express.

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ var express = require ('express');
 var app = express();
 
 var port = 4000;
-app.get ('/', function (req, res){
-    res.send('Simple Routing Test');
+
+app.get('/', function(req,res){
+    res.send('Hello Home Page');
+});
+
+app.get('/guard', function(req,res){
+    res.send('Hello Guard');
+});
+
+app.get('/passing', function(req,res){
+    res.send('Hello Passing');
 });
 
 app.listen(port,function(err){
