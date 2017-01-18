c5382b5afa2ef9c96b1d1aae8e1f87dbbe344bf6
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-01-18 13:51:19 -0500

    Add routing

--- app.js
+++ app.js
@@ -2,13 +2,75 @@ var express = require ('express');
 
 var app = express();
 
+
 var port = 4000;
 
-app.get('/', function(req,res){
-    res.send('Hello Home Page update test');
+var nav = [{
+    Link:'/guard', 
+    Text: 'Guard'
+        }, {
+    Link:'/topgame', 
+    Text: 'Top Game'
+        }, {
+    Link: '/escapes', 
+    Text: 'Escapes'
+        }, { 
+    Link:'/takedowns', 
+    Text: 'Takedowns'
+}]
+;
+var guardRouter = express.Router();
+
+app.use(express.static('public'));
+
+app.set('views', 'src/views');
+app.set('view engine', 'ejs');
+
+app.use ('/guard', guardRouter);
+
+guardRouter.route('/')
+    .get(function(req, res) {
+        res.render('guard'), {
+        title : 'render check', 
+        nav: [{
+            Link:'/guard', 
+            Text: 'Guard'
+        }, {
+            Link:'/topgame', 
+            Text: 'Top Game'
+        }, {
+            Link: '/escapes', 
+            Text: 'Escapes'
+        }, { 
+            Link:'/takedowns', 
+            Text: 'Takedowns'
+        }]
+     };
 });
-app.use (express.static('public'));
-app.use (express.static('src/views'));
+
+    guardRouter.route('/half')
+    .get(function(req, res) {
+        res.send('hello half guard');
+    });
+
+app.get('/', function(req,res){
+    res.render('index', {
+        title : 'render check', 
+        nav: [{
+            Link:'/guard', 
+            Text: 'Guard'
+        }, {
+            Link:'/topgame', 
+            Text: 'Top Game'
+        }, {
+            Link: '/escapes', 
+            Text: 'Escapes'
+        }, { 
+            Link:'/takedowns', 
+            Text: 'Takedowns'
+        }]
+    });
+});
 
 app.get('/guard', function(req,res){
     res.send('Hello Guard');
@@ -18,8 +80,8 @@ app.get('/topgame', function(req,res){
     res.send('Hello Top Game');
 });
 
-app.get('/buttomgame', function(req,res){
-    res.send('Hello Buttom Game');
+app.get('/escapes', function(req,res){
+    res.send('Hello Escapes');
 });
 
 app.get('/takedowns', function(req,res){
