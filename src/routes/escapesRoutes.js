ed9a2aab296616841ac85b79608c1b7a3c6d481f
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-02-02 15:27:25 -0500

    Reformatting re jscsrc rules

--- src/routes/escapesRoutes.js
+++ src/routes/escapesRoutes.js
@@ -8,33 +8,36 @@ var router = function(nav){
         title: 'Hitchhicker',
         description: 'Widely used against armbar from the mount.',
         attack: 'Armbar',
-        examples: 'http://www.w3schools.com/'
-        
-        },
+        examples: 'https://www.youtube.com/watch?v=ShV9jUhbyLA'
+    },
     {
         title: 'Triangle Spin Out',
         description:  'A strong escape against a leg triangle submission from the guard.',
         attack: 'Triangle',
-        
-        },
+        examples: 'https://www.youtube.com/watch?v=szAvJFvIzx4'
+
+    },
     {
         title: 'Kimura spin under',
         description:  'Used to counter a kimura submission from side control or north-south.',
         attack: 'Kimura',
-        
-        },
+        examples: 'https://www.youtube.com/watch?v=AvtbqzGBVDA&list=PLRWboZ2QTrrtVqF0sQCzQgp4NvqFwNMKd&index=15'
+
+    },
     {
-        title: 'Choke block',
+        title: 'Choke defense',
         description: 'A strategy to avoid being choked from the back.',
         attack: 'RNC',
-        
-        },
+        examples: 'https://www.youtube.com/watch?v=JR2e0IsqhrQ'
+
+    },
     {
         title: 'Footlook escape',
-        description: 'One way to disentangle your legs and counter a basic ankle lock.',
+        description: 'One strategy to disentangle your legs and counter a basic ankle lock.',
         attack: 'Ankle lock',
-        
-        }
+        examples: 'https://www.youtube.com/watch?v=_VUybC7nCuE'
+
+    }
     ];
     escapeRouter.route('/')
     .get(function (req, res) {
