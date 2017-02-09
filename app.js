81b68d9f0a1e7171b440c2615647627d4eba44f2
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-02-09 15:57:17 -0500

    Add file
    Version for error screening

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 var express = require('express');
+//var bodyParser = require('body-parser');
 
 var app = express();
 
-var port = 4000;
+var port = process.env.PORT || 4000;
 var nav = [{
     Link: '/escapes',
     Text: 'Escapes'
-    }, {
+},
+{
     Link: '/attacks',
     Text: 'By attack'
-    }];
+}];
 var escapesRouter = require('./src/routes/escapesRoutes')(nav);
+//var adminRouter = require('./src/routes/adminRoutes')(nav);
+//var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
-app.set('views', './src/views');
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
 
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-
 app.use('/escapes', escapesRouter);
+//app.use('/admin', adminRouter);
+//app.use('/auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
         title: 'Hello from render',
         nav: [{
             Link: '/escapes',
-            Text: 'escapes'
+            Text: 'Escapes'
         }, {
             Link: '/Attacks',
             Text: 'By Attack'
@@ -35,6 +42,10 @@ app.get('/', function (req, res) {
 
 app.get('/escapes', function (req, res) {
     res.send('Hello escapes');
+});
+
+app.get('/attacks', function (req, res) {
+    res.send('Coming Soon');
 });
 
 app.listen(port, function (err) {
