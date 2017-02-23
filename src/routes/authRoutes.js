c48d1b9902e4cecdc955148f6fc111247c6dcb9a
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-02-23 11:31:32 -0500

    Sign in configuration for mongo/passport

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var passport = require('passport');
 
 var router = function () {
     authRouter.route('/signUp')
@@ -11,19 +12,31 @@ var router = function () {
             mongodb.connect(url, function(err, db) {
             var collection = db.collection('users');
             var user = {
-                username: req.body.username,
+                username: req.body.userName,
                 password: req.body.password
             };
             collection.insert(user, function (err, results)
             {
-                req.login (results, function() {
+                req.login (results.ops[0], function() {
                     res.redirect('/auth/profile');
                 });
             });
         });
         });
+    authRouter.route('/signIn')
+        .post(passport.authenticate('local', {
+            failureRedirect: '/'
+        }), function (req, res) {
+                res.redirect('/auth/profile');
+            });
     authRouter.route('/profile')
-        .get(function(req, res) {
+        .all(function (req, res, next) {
+            if (!req.user) {
+                res.redirect('/');
+            }
+            next();
+        })
+        .get(function (req, res) {
             res.json(req.user);
         });
     return authRouter;
